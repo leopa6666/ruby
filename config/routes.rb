69072d5e4622e96589396b0a5da6bac10c63a0a9
leopa6666 <40862417+leopa6666@users.noreply.github.com>
2019-11-10 20:48:47 +0900

    sclayping

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'nokogiri#testsc'
+  root 'top#index'
   post 'index/testbtn'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
