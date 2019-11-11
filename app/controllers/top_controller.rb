114ec2b88a4348bacd9a337e83dced3df70feea8
leopa6666 <40862417+leopa6666@users.noreply.github.com>
2019-11-11 21:55:20 +0900

    sclayping

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -29,26 +29,27 @@ class TopController < ApplicationController
       
     charset = nil
 
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
-    end
-
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-
-
     # CSVヘッダー
     headers = ['Body', 'Page URL']
-
     csv_data = CSV.generate do |csv|
-      # 一行目はCSVヘッダーにする
-      csv << headers
 
-      doc.xpath('//article[@id="post-single"]').each do |node|
-        csv << [node.css('h1').inner_text, url]
-        csv << [node.css('p').inner_text, url]
-        #p node.css('p').inner_text
-      end 
+      urls.each  do |url|
+        sleep(6)
+        html = open(url) do |f|
+          charset = f.charset
+          f.read
+        end
+    
+        doc = Nokogiri::HTML.parse(html, nil, charset)
+        # 一行目はCSVヘッダーにする
+        csv << headers
+
+        doc.xpath('//article[@id="post-single"]').each do |node|
+          csv << [node.css('h1').inner_text, url]
+          csv << [node.css('p').inner_text, url]
+          #p node.css('p').inner_text
+        end 
+      end
     end
     send_data(csv_data, filename: "posts.csv")
   end
